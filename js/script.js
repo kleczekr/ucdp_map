d6b464af90543579f886f742fc1668e556f01d16
kleczekr <kleczek.r@gmail.com>
2022-08-22 00:15:44 +0200

    Started on the tooltip for the map

--- js/script.js
+++ js/script.js
@@ -11,17 +11,52 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 const svg = d3.select(map.getPanes().overlayPane).append("svg");
 const g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-// const get_coords = (d) => {map.latLngToLayerPoint([+data.latitude, +data.longitude]);}
+// tooltip for the circles
+const tooltip = d3.select("#map-tooltip");
+
+// a function to add hover listener with a tooltip over circles
+const addHoverListener = (circle, tooltip) => {
+  circle
+    .on("mouseover", function (d) {
+      console.log(d);
+      console.log(d.target.options.title);
+      tooltip.transition().duration(200).style("opacity", 1);
+      tooltip
+        .html(d.target.options.title)
+        .style("left", d.originalEvent.pageX + "px")
+        .style("top", d.originalEvent.pageY - 28 + "px");
+    })
+    .on("mouseout", function (d) {
+      tooltip.transition().duration(500).style("opacity", 0);
+    })
+    .on("mousemove", function (d) {
+      tooltip
+        .style("left", d.originalEvent.pageX + "px")
+        .style("top", d.originalEvent.pageY - 28 + "px");
+    })
+    .on("click", function (d) {
+      console.log(d.target.options.title);
+    })
+    .on("dblclick", function (d) {
+      console.log(d.target.options.title);
+    });
+};
 
 // read the CSV for the data and add the points to the map
 d3.csv("data/sl.csv", (data) => {
   // use the map.latLngToLayerPoint function to get the coordinates for the points
   // const coords = map.latLngToLayerPoint([+data.latitude, +data.longitude]);
+  console.log(data);
   const marker = L.circle([+data.latitude, +data.longitude], {
     color: "#C54B6C",
     fillColor: "#C54B6C",
-    opacity: 0.3,
+    opacity: 0.6,
     fillOpacity: 0.3,
     radius: 15,
+    title: `Title: ${data.conflict_name}<br>Date: ${new Date(
+      data.date_start
+    ).toLocaleDateString("af-ZA")}`,
   }).addTo(map);
+
+  addHoverListener(marker, tooltip);
 });
