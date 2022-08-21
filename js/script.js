41d45145b8a7f6c0593135abda512691b4f78e0b
kleczekr <kleczek.r@gmail.com>
2022-08-21 23:33:30 +0200

    Focused on SL data, adjusted the map and points

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // initialize a map in the 'map' div, set the view to a given place and zoom
-const map = L.map("map").setView([0, 0], 2);
+const map = L.map("map").setView([7.87, 80.77], 7);
 
 // initialize the title layer and settings for the map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -14,25 +14,14 @@ const g = svg.append("g").attr("class", "leaflet-zoom-hide");
 // const get_coords = (d) => {map.latLngToLayerPoint([+data.latitude, +data.longitude]);}
 
 // read the CSV for the data and add the points to the map
-d3.csv("data/ucdp.csv", (data) => {
+d3.csv("data/sl.csv", (data) => {
   // use the map.latLngToLayerPoint function to get the coordinates for the points
   // const coords = map.latLngToLayerPoint([+data.latitude, +data.longitude]);
   const marker = L.circle([+data.latitude, +data.longitude], {
-    color: "red",
-    fillColor: "#f03",
-    fillOpacity: 0.5,
-    radius: 5,
+    color: "#C54B6C",
+    fillColor: "#C54B6C",
+    opacity: 0.3,
+    fillOpacity: 0.3,
+    radius: 15,
   }).addTo(map);
-  // console.log(coords);
-  // let Circles = svg
-  //   .selectAll("circle")
-  //   .data(data)
-  //   .enter()
-  //   .append("circle")
-  //   .attr("cx", coords.x)
-  //   .attr("cy", coords.y)
-  //   .attr("r", 5)
-  //   .style("stroke", "black")
-  //   .style("opacity", 0.6)
-  //   .style("fill", "yellow");
 });
