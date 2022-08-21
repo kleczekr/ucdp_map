4ce22584cdd11e9371da6da183c50d211b387ca0
kleczekr <kleczek.r@gmail.com>
2022-08-21 02:06:52 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const map = L.map("map").setView([0, 0], 2);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
